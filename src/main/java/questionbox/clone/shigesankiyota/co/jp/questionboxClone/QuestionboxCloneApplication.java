f709967886bc1601bafbe13d0b515666c50002e4
shige <sy20151201@gmail.com>
2021-04-10 17:56:14 +0900

    initial commit

---
+++ src/main/java/questionbox/clone/shigesankiyota/co/jp/questionboxClone/QuestionboxCloneApplication.java
@@ -0,0 +1,13 @@
+package questionbox.clone.shigesankiyota.co.jp.questionboxClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionboxCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionboxCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/questionbox/clone/shigesankiyota/co/jp/questionboxClone/QuestionboxCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package questionbox.clone.shigesankiyota.co.jp.questionboxClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionboxCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
