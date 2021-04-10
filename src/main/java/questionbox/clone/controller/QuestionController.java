fb75a00325a7bf683b2f0329ff2f8630410e8b8a
shige <sy20151201@gmail.com>
2021-04-11 01:16:09 +0900

    * 不要そうなコメントアウトを外した * 不要な import の記述を削除

--- src/main/java/questionbox/clone/controller/QuestionController.java
+++ src/main/java/questionbox/clone/controller/QuestionController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 import questionbox.clone.entity.Question;
 import questionbox.clone.service.QuestionService;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -17,7 +16,7 @@ public class QuestionController {
 	final QuestionService service;
 
 	@GetMapping("/list")
-	// @ResponseBody
+	@ResponseBody
 	public List<Question> list() {
 		// TODO 実装
 		// var question = new Question(2, "質問", "回答", "やまざき", "きよた", true, true);
--- src/test/java/questionbox/clone/controller/QuestionControllerTests.java
+++ src/test/java/questionbox/clone/controller/QuestionControllerTests.java
@@ -1,16 +1,12 @@
 package questionbox.clone.controller;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import questionbox.clone.QuestionBoxCloneApplication;
 import questionbox.clone.entity.Question;
 import questionbox.clone.service.QuestionService;
 
@@ -23,7 +19,6 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWit
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(QuestionController.class)
@@ -40,11 +35,9 @@ class QuestionControllerTests {
 	void shouldReturnQuestionList() throws Exception {
 
 		var question = new Question(1, "質問", "回答", "やまざき", "きよた", true, true);
-		var question2 = new Question(2, "質問", "回答", "きよた", "やまざき", false, false);
-		List<Question> questions = Arrays.asList(question, question2);
-
+		List<Question> questions = Arrays.asList(question);
 		when(service.findAll()).thenReturn(questions);
-		//
+
 		this.mockMvc.perform(get("/list") //
 				.accept(MediaType.APPLICATION_JSON)) //
 				.andDo(print()) //
