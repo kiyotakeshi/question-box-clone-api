e273e77d6fe6a19c075b07737325aacb6da90fab
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-04-10 20:35:09 +0900

    flyway による migration の確認
    ```shell
    docker-compose exec mysql bash -c 'mysql -u question-box -ppassword question-box -e "select * from question;";'
    
    mysql: [Warning] Using a password on the command line interface can be insecure.
    +----+----------+----------+--------+----------+------------+------------+
    | id | answered | archived | answer | question | questioner | respondent |
    +----+----------+----------+--------+----------+------------+------------+
    |  1 |        1 |        1 | ??     | ????     | ????       | ???        |
    +----+----------+----------+--------+----------+------------+------------+
    ```

---
+++ src/main/resources/db/migration/V1.0.0__create_table.sql
@@ -0,0 +1,1 @@
+create table question (id integer not null, answered boolean not null, archived boolean not null, answer varchar(255), question varchar(255), questioner varchar(255), respondent varchar(255), primary key (id))
